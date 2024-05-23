b098c5bd073053e05be44b42c6e8950038084ebe
Marlos <marlossh88@gmail.com>
2024-05-22 21:22:04 -0300

    Recriando Interfaces Frm Gerenciar e Cadastros

---
+++ src/main/java/com/project/visual/FrmGerenciarFerramenta.java
@@ -0,0 +1,46 @@
+/*
+ * Created by JFormDesigner on Wed May 22 20:55:05 BRT 2024
+ */
+
+package com.project.visual;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author lucas
+ */
+public class FrmGerenciarFerramenta extends JPanel {
+    public FrmGerenciarFerramenta() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Marlos
+
+        //======== this ========
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+        EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing
+        . border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ),
+        java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+        { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () ))
+        throw new RuntimeException( ); }} );
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Marlos
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
